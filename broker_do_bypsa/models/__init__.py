f1d06058a8fbed34c9a8f7dc20f5fbc1e8e6ae26
Luis Rocha <luis.rocha@tandicorp.net>
2024-06-30 15:56:37 -0500

    LROCHA: Se corrige por referencia circular en vistas. Configuración innecesaria de vistas en tipos de endoso

--- broker_do_bypsa/models/__init__.py
+++ broker_do_bypsa/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#from . import broker_contract
-#from . import sale_order
-#from . import broker_movement_object
-#from . import broker_depreciation
+from . import broker_contract
+from . import sale_order
+from . import broker_movement_object
+from . import broker_depreciation
